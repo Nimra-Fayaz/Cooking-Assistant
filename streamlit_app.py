26c11d23f5af0a896935a8ba2050e296503fd43a
Nimra-Fayaz <142812663+Nimra-Fayaz@users.noreply.github.com>
2023-08-27 13:24:06 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,9 +72,9 @@ def main():
     uploaded_file = st.file_uploader("Upload the Image", type=["jpg", "jpeg", "png"])
     
     # Check if an image has been uploaded
-    if uploaded_file is not None:
-        # Call the get_ingredients function with the uploaded image
-        predicted_ingredients = get_ingredients(uploaded_file.read())
+   uploaded_file = st.file_uploader("Upload the Image", type=["jpg", "jpeg", "png"])
+   if uploaded_file is not None:
+    predicted_ingredients = get_ingredients(uploaded_file.read())
         
         if st.button("Get Recipes"):
             recipes = get_recipes(predicted_ingredients)
