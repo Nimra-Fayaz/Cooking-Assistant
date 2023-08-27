bbb85d8cd65a0fab7bd741bbbeded86dede422b5
Nimra-Fayaz <142812663+Nimra-Fayaz@users.noreply.github.com>
2023-08-27 12:11:24 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,6 +44,7 @@ def get_recipes(predicted_ingredients):
 
 # Streamlit UI
 import streamlit as st
+def main():
      # Initialize session_state
 if 'predicted_ingredients' not in st.session_state:
     st.session_state.predicted_ingredients = []
@@ -54,12 +55,14 @@ st.set_page_config(page_title="Your Cooking Assistant", page_icon="🍳", layout
 st.title("Your Cooking Assistant")
 # File uploader widget
 uploaded_file = st.file_uploader("Upload the Image", type=["jpg", "jpeg", "png"])
-    # Check if an image has been uploaded
+# Check if an image has been uploaded
 if uploaded_file is not None:
     # Call the get_ingredients function with the uploaded image
-    predicted_ingredients = get_ingredients(image.read())
+    predicted_ingredients = get_ingredients(uploaded_file.read())
     if st.button("Get Recipes"):
        recipes = get_recipes(predicted_ingredients)
        st.success("Here are some recipe ideas:")
        st.write(recipes)
        st.write(predicted_ingredients)
+if __name__ == "__main__":
+    main()
